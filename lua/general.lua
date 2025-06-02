e984fae7db184f7f27b4f871ff07ee0e5165d731
lurkzies <lrkzies@proton.me>
2025-06-02 13:40:22 -0400

    Removed use of arrow keys

--- lua/general.lua
+++ lua/general.lua
@@ -14,6 +14,21 @@ vim.opt.background = "dark"       -- Set background preference for themes
 vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 -- Nvim-Telescope
 vim.api.nvim_set_keymap('n', '<leader>f', ':Telescope find_files<CR>', { noremap = true, silent = true})
+-- Remove arrow key navigation (Normal Mode)
+vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', { noremap = true, silent = true})
+-- Remove arrow key navigation (Insert Mode)
+vim.api.nvim_set_keymap('i', '<Up>', '<Nop>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<Down>', '<Nop>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<Left>', '<Nop>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<Right>', '<Nop>', { noremap = true, silent = true})
+-- Remove arrow key navigation (Visual Mode)
+vim.api.nvim_set_keymap('v', '<Up>', '<Nop>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<Down>', '<Nop>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<Left>', '<Nop>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<Right>', '<Nop>', { noremap = true, silent = true})
 
 -- Tab indenting
 vim.opt.tabstop = 2       -- Number of spaces that a tab character represents
