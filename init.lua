dbf875ec39f5cc667bca4c19a61ddfb487723acb
lurkzies <lrkzies@proton.me>
2025-07-16 13:26:09 -0400

    Updated TS Update for Lazy

--- init.lua
+++ init.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
   { -- Treesitter
     "nvim-treesitter/nvim-treesitter",
     event = "BufRead",
-    run = ":TSUpdate",
+    build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
         highlight = { enable = true },
