9dcdba41b0388ac88addfdf85503eaec2b933240
lurkzies <lrkzies@proton.me>
2026-06-05 12:10:50 -0400

    Fixed deprecated syntax error (lua/lsp.lua)

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,5 +1,5 @@
 -- lsp-zero v3  ✦  mason-lspconfig 1.x  ✦  Neovim 0.9+
-local lsp         = require("lsp-zero")
+local lsp          = require("lsp-zero")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 require("mason").setup()
@@ -7,14 +7,14 @@ require("mason").setup()
 require("mason-lspconfig").setup({
   ensure_installed = {
     "lua_ls",
-    "ts_ls",                       -- <- new TypeScript name
+    "ts_ls", -- <- new TypeScript name
     "clangd",
     "pyright",
     "html",
     "cssls",
     "docker_compose_language_service",
   },
-  automatic_installation = true,   -- ← works in v1, harmless in 1.*
+  automatic_installation = true, -- ← works in v1, harmless in 1.*
   handlers = {
     function(server)
       require("lspconfig")[server].setup({
@@ -35,7 +35,7 @@ require("mason-lspconfig").setup({
       require("lspconfig").ts_ls.setup({
         on_attach    = lsp.on_attach,
         capabilities = capabilities,
-        settings = {
+        settings     = {
           typescript = { format = { indentSize = 2, tabSize = 2 } },
           javascript = { format = { indentSize = 2, tabSize = 2 } },
         },
@@ -47,18 +47,18 @@ require("mason-lspconfig").setup({
 -- common key-maps + on-save formatting
 lsp.on_attach(function(client, bufnr)
   local o = { buffer = bufnr }
-  vim.keymap.set("n", "gd",         vim.lsp.buf.definition,    o)
-  vim.keymap.set("n", "K",          vim.lsp.buf.hover,         o)
-  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action,   o)
-  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename,        o)
-  vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references,    o)
-  vim.keymap.set("n", "<leader>d",  vim.diagnostic.open_float, o)
-  vim.keymap.set("n", "<leader>f",  function() vim.lsp.buf.format({ async = true }) end, o)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, o)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, o)
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, o)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, o)
+  vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, o)
+  vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, o)
+  vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format({ async = true }) end, o)
 
-  if client.supports_method("textDocument/formatting") then
+  if client:supports_method("textDocument/formatting") then
     vim.api.nvim_create_autocmd("BufWritePre", {
-      group   = vim.api.nvim_create_augroup("LspFormat." .. bufnr, { clear = true }),
-      buffer  = bufnr,
+      group    = vim.api.nvim_create_augroup("LspFormat." .. bufnr, { clear = true }),
+      buffer   = bufnr,
       callback = function() vim.lsp.buf.format({ async = false }) end,
     })
   end
@@ -68,10 +68,10 @@ end)
 local nil_path = vim.fn.exepath("nil")
 if nil_path ~= "" then
   require("lspconfig").nil_ls.setup({
-    cmd         = { nil_path },
-    on_attach   = lsp.on_attach,
+    cmd          = { nil_path },
+    on_attach    = lsp.on_attach,
     capabilities = capabilities,
-    filetypes   = { "nix" },
-    root_dir    = require("lspconfig.util").root_pattern(".git", "flake.nix"),
+    filetypes    = { "nix" },
+    root_dir     = require("lspconfig.util").root_pattern(".git", "flake.nix"),
   })
 end
